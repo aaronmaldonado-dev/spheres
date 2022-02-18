f6746a7051a6f1244c89c591a5da49636b9caa9b
Sekam Dex <24422249+sekamdex@users.noreply.github.com>
2022-02-17 19:06:43 -0600

    added gsap

--- src/components/Sphere/Sphere.jsx
+++ src/components/Sphere/Sphere.jsx
@@ -1,10 +1,24 @@
 import { useRef } from "react";
+import gsap from "gsap";
 
 export const Sphere = ({ color, note, playNote }) => {
   const mesh = useRef();
 
   const onClickHandler = (e) => {
     e.stopPropagation();
+    const scale = getRandomArbitrary(1.25, 2);
+
+    // there's a bug that I actually like,
+    // click two consecutive times and sphere is gonna keep the target scale
+    gsap.to(mesh.current.scale, {
+      duration: 0.25,
+      x: scale,
+      y: scale,
+      z: scale,
+      yoyo: true,
+      repeat: 1
+    });
+
     playNote(note);
   };
 
